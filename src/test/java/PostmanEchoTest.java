c1693a71f07e5eb0eb0776355ae696fe242c8649
Maria Sedova <mariassedova@mail.ru>
2021-05-18 21:14:18 +0300

    test fixed

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -15,7 +15,7 @@ public class PostmanEchoTest {
                     .post("/post")
                     .then()
                     .statusCode(200)
-                    .body("data", equalTo("Test message"))
+                    .body("data", equalTo("some data"))
             ;
 
     }
