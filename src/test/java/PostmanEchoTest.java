d233f9c9615877533499a61297cd7f9613774967
Maria Sedova <mariassedova@mail.ru>
2021-05-18 21:12:44 +0300

    incorrect test

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -1,27 +1,22 @@
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.form;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEchoTest {
 
-    class PostmanEchoTests {
         @Test
-        void shouldShowSomeData() {
-            // Given - When - Then
-// Предусловия
+        void shouldShowSomeInfo() {
             given()
                     .baseUri("https://postman-echo.com")
-                    .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
-// Выполняемые действия
+                    .body("some data")
                     .when()
                     .post("/post")
-// Проверки
                     .then()
                     .statusCode(200)
-                    .body("data", equalTo("some data"))
+                    .body("data", equalTo("Test message"))
             ;
-        }
 
     }
 
